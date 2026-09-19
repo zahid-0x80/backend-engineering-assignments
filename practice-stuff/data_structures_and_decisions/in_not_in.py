98dc4ea64077eeffd7ee4bd528b270b2651fb3cc
zahid-0x80 <zahid0x01@gmail.com>
2026-09-19 12:24:09 +0600

    checking a value whether a value exists in a collection or not:in_not_in.py

---
+++ practice-stuff/data_structures_and_decisions/in_not_in.py
@@ -0,0 +1,5 @@
+# in and not in checks whether a value exists in a collection or not.
+names = ["Zahid Hasan", "Kenny", "Scarlett Johansson"]
+print("Kenny" in names) # returns True
+print("Julia Roberts" in names) # returns False
+print("Ashley Judd" not in names) # returns True
